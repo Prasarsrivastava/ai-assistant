daa99c5325cdcb4a5d19021f8f67c6fe10d74260
Prasar <prasarsrivastava@gmail.com>
2025-09-14 19:13:48 +0530

    first setup

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+dotenv.config()
+
+const app=express()
+const port=process.env.PORT
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRouter)
+// app.get("/",(req,res)=>{
+//     res.send("Hello 2783 World")
+// })
+
+app.listen(port,()=>{
+    connectDB()
+    console.log(`Server is running on port `);
+})
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import e from "express";
+import React from "react";
+
+function SignUp() {
+  return (
+    <div >Sign Up Page</div>
+  )
+}
+export default SignUp;
